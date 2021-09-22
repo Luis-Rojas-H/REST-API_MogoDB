ca79eb410d30fe867a41f730deae6029553aee3e
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-09-22 10:50:22 -0500

    Inicio Preparado
    instalación de dependencias.
    No olvidar iniciar primero mongod

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const expres = require('express')
+const mongoose = require('mongoose')
+
+const app = expres()
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/rest-api-example')
+    .then(db => console.log('db is connected'))
+    .catch(err => console.log(err))
+
+// settings
+app.set('port', process.env.PORT || 3000)
+
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+// routes
+
+// start server
+app.listen(app.get('port'), () => {
+        console.log(`server on port `, app.get('port'));
+})
